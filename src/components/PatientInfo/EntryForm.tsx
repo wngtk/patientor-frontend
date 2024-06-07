5f0899cdf9fe108fe29ffeb22db5b1087a35b2ea
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-07 20:12:59 +0800

    9.28: Patientor, step 9

--- src/components/PatientInfo/EntryForm.tsx
+++ src/components/PatientInfo/EntryForm.tsx
@@ -1,49 +1,116 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import { SyntheticEvent } from "react";
-import { HealthCheckEntry, Patient } from "../../types";
+import {
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { SyntheticEvent, useState } from "react";
+import { EntryWithoutId, HealthCheckEntry, HospitalEntry, OccupationalHealthcareEntry, Patient } from "../../types";
 import patientsService from "../../services/patients";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 // 做类型体操啦！
 // 把原本想要的字段类型转换成每个字段都是一个对象里面有个 value
 type Target<Type> = {
-  [Property in keyof Type]: { value: string }
-}
+  [Property in keyof Type]: Property extends keyof Type[Property] ? Target<Property> :  { value: string } ;
+};
 
-type HealthCheckEntryWithoutId = Omit<HealthCheckEntry, 'id'>
+type HealthCheckEntryWithoutId = Omit<HealthCheckEntry, "id">;
+type HospitalEntryWithoutId = Omit<HospitalEntry, "id">;
+type OccupationalHealthcareEntryWithoutId = Omit<OccupationalHealthcareEntry, "id">;
 
 interface Props {
-  patientId: string,
-  setPatient: React.Dispatch<React.SetStateAction<Patient | null>>,
-  setError: (message: string) => void
+  patientId: string;
+  setPatient: React.Dispatch<React.SetStateAction<Patient | null>>;
+  setError: (message: string) => void;
 }
 
 const EntryForm = ({ patientId, setPatient, setError }: Props) => {
+  const [entryType, setEntryType] = useState("HealthCheck");
+
   const onSubmit = async (event: SyntheticEvent) => {
-    event.preventDefault()
-    const target = event.target as typeof event.target & Target<HealthCheckEntryWithoutId>;
+    event.preventDefault();
+    
+    let entry: EntryWithoutId
+    
+    if (entryType === "HealthCheck") {
+      const target = event.target as typeof event.target &
+        Target<HealthCheckEntryWithoutId>;
 
-    const newEntry: HealthCheckEntryWithoutId = {
-      date: target.date.value,
-      description: target.description.value,
-      healthCheckRating: Number(target.healthCheckRating.value),
-      specialist: target.specialist.value,
-      type: "HealthCheck",
+      const newEntry: HealthCheckEntryWithoutId = {
+        date: target.date.value,
+        description: target.description.value,
+        healthCheckRating: Number(target.healthCheckRating.value),
+        specialist: target.specialist.value,
+        type: "HealthCheck",
+      };
+  
+      if (target.diagnosisCodes?.value) {
+        newEntry.diagnosisCodes = target.diagnosisCodes.value.split(",");
+      }
+
+      entry = newEntry
+    } else if (entryType === "Hospital") {
+      const target = event.target as typeof event.target & Target<HospitalEntryWithoutId> & {
+        dischargeCriteria: { value: string},
+        dischargeDate: { value: string },
+      }
+      const newEntry: HospitalEntryWithoutId = {
+        date: target.date.value,
+        description: target.description.value,
+        specialist: target.specialist.value,
+        type: "Hospital",
+        discharge: {
+          criteria: target.dischargeCriteria.value,
+          date: target.dischargeDate.value
+        }
+      }
+
+      if (target.diagnosisCodes?.value) {
+        newEntry.diagnosisCodes = target.diagnosisCodes.value.split(",");
+      }
+
+      entry = newEntry
+    } else if (entryType === "OccupationalHealthcare") {
+      const target = event.target as typeof event.target & Target<OccupationalHealthcareEntryWithoutId> & {
+        sickLeaveStartDate: { value: string},
+        sickLeaveEndDate: { value: string}
+      }
+      const newEntry: OccupationalHealthcareEntryWithoutId = {
+        date: target.date.value,
+        description: target.description.value,
+        specialist: target.specialist.value,
+        type: "OccupationalHealthcare",
+        employerName: target.employerName.value
+      }
+      if (target.diagnosisCodes?.value) {
+        newEntry.diagnosisCodes = target.diagnosisCodes.value.split(",");
+      }
+
+      if (target.sickLeaveStartDate.value && target.sickLeaveEndDate.value) {
+        newEntry.sickLeave = {
+          startDate: target.sickLeaveStartDate.value,
+          endDate: target.sickLeaveEndDate.value
+        }
+      }
+
+      entry = newEntry      
+    } else {
+      throw new Error('Unsupported entry type')
     }
-
-    if (target.diagnosisCodes?.value) {
-      newEntry.diagnosisCodes = target.diagnosisCodes.value.split(',')
-    }
-
-    console.log(newEntry)
 
     try {
-      const data = await patientsService.addEntry(patientId, newEntry)
-      setPatient(data)
+      const data = await patientsService.addEntry(patientId, entry);
+      setPatient(data);
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
-        if (error.response?.data && typeof error.response.data.error === 'string') {
-          setError(error.response.data.error)
+        if (
+          error.response?.data &&
+          typeof error.response.data.error === "string"
+        ) {
+          setError(error.response.data.error);
         }
       }
     }
@@ -52,18 +119,85 @@ const EntryForm = ({ patientId, setPatient, setError }: Props) => {
   return (
     <form onSubmit={onSubmit}>
       <Stack mt={2} p={2} sx={{ border: "2px dashed grey" }}>
-        <Typography fontWeight="bolder" pt={2} pb={2}>
-          New HealthCheck entry
-        </Typography>
-        <TextField id="standard-basic" label="Description" variant="standard" name="description" />
-        <TextField id="standard-basic" label="Date" variant="standard" name="date" />
-        <TextField id="standard-basic" label="Specialist" variant="standard" name="specialist" />
+        <Box fontWeight="bolder" pt={2} pb={2}>
+          New{" "}
+          <Select
+            id="demo-simple-select-standard"
+            value={entryType}
+            onChange={({ target }) => setEntryType(target.value)}
+          >
+            <MenuItem value="HealthCheck">HealthCheck</MenuItem>
+            <MenuItem value="Hospital">Hospital</MenuItem>
+            <MenuItem value="OccupationalHealthcare">
+              OccupationalHealthcare
+            </MenuItem>
+          </Select>{" "}
+          entry
+        </Box>
         <TextField
           id="standard-basic"
-          label="HealthCheck rating"
+          label="Description"
           variant="standard"
-          name="healthCheckRating"
+          name="description"
         />
+        <TextField
+          id="standard-basic"
+          label="Date"
+          variant="standard"
+          name="date"
+        />
+        <TextField
+          id="standard-basic"
+          label="Specialist"
+          variant="standard"
+          name="specialist"
+        />
+        {entryType === "HealthCheck" && (
+          <TextField
+            id="standard-basic"
+            label="HealthCheck rating"
+            variant="standard"
+            name="healthCheckRating"
+          />
+        )}
+        {entryType === "OccupationalHealthcare" && (
+          <>
+            <TextField
+              id="standard-basic"
+              label="Employ Name"
+              variant="standard"
+              name="employName"
+            />
+            <TextField
+              id="standard-basic"
+              label="Sick Leave Start Date"
+              variant="standard"
+              name="sickLeaveStartDate"
+            />
+            <TextField
+              id="standard-basic"
+              label="Sick Leave End Date"
+              variant="standard"
+              name="sickLeaveEndDate"
+            />
+          </>
+        )}
+        {entryType === "Hospital" && (
+          <>
+            <TextField
+              id="standard-basic"
+              label="Discharge date"
+              variant="standard"
+              name="dischargeDate"
+            />
+            <TextField
+              id="standard-basic"
+              label="Discharge criteria"
+              variant="standard"
+              name="dischargeCriteria"
+            />
+          </>
+        )}
         <TextField
           id="standard-basic"
           label="Diagnosis codes"
